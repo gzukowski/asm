735a01660249a596c8826bc9f80c69190f9b2058
gzukowski <85632612+gzukowski@users.noreply.github.com>
2024-03-13 22:38:31 +0100

    Server side task gen added

--- exercises/stack-sim-online/simulator.py
+++ exercises/stack-sim-online/simulator.py
@@ -1,9 +1,13 @@
 import random
+from tkinter import messagebox
 import requests
 import tkinter as tk
 from tkinter import font
+import sys
 
-BASE_URL = "http://34.17.58.255:8080/"
+
+
+BASE_URL = "http://127.0.0.1:8080/"
 GROWTH = {"dec": 1, "inc": -1}
 REGISTER = {"EAX": 4, "AX": 2}
 EAX = 4  # bytes
@@ -20,15 +24,19 @@ class Simulator(tk.Tk):
         self.title("Stacksim")
         self.geometry("640x480")
         self.configure(bg="light gray")
-        self.growth = random.choice(list(GROWTH.keys()))
         custom_font = font.Font(family="Helvetica", size=16)
+
         self.register = "EAX"
         self.adress_labels = []
         self.values_labels = []
-        self.refresh_cells()
-        self.refresh_values()
+        self.task_id = 0
 
 
+        success = self.get_new_task()
+
+        if not success:
+            messagebox.showerror("Error", "Failed to load, check internet.")
+            return
 
         for index, value in enumerate(self.cells):
             address_label = tk.Label(self, text=f"{value}:".upper(), padx=10, font=custom_font, width=8)
@@ -46,7 +54,7 @@ class Simulator(tk.Tk):
         self.stack_pointer_label = tk.Label(self, text=f"{hex(self.stack_pointer)}".upper(), padx=10, font=custom_font)
         self.stack_pointer_label.grid(row=9, columnspan=1, pady=10)
 
-        self.pointer_label = tk.Label(self, text=f"<-", font=custom_font)
+        self.pointer_label = tk.Label(self, text="<-", font=custom_font)
         self.pointer_label.grid(row=9, column=1)
 
         self.entry = tk.Entry(self, width=20,)
@@ -55,42 +63,45 @@ class Simulator(tk.Tk):
         self.submit_button = tk.Button(self, text="Submit", command=self.check)
         self.submit_button.grid(row=9, column=3)
 
-        self.exercise_test_label = tk.Label(self, text=f"Stan bezposrednio po push EAX, \n jaka wartosc bedzie w EAX po wykonaniu pop EAX", padx=10, font=custom_font)
+        self.exercise_test_label = tk.Label(self, text="State directly after push EAX, \n what will be the value of EAX after pop EAX", padx=10, font=custom_font)
         self.exercise_test_label.grid(row=10, column=0, columnspan=5)
+
+
 
     def check(self):
         headers = {'Content-Type': 'application/json',
                    'Authorization': f'Token {self.TOKEN}'
                    }
-        data = {}
-
-        user_input = self.entry.get().upper()
-        resulting_string = "".join(self.answer).upper()
+        data = {
+            "answer" : self.entry.get().upper(),
+            "task_id" : self.task_id
+        }
+    
         self.submit_button.config(state=tk.DISABLED, relief=tk.SUNKEN, background='gray')
-        
-        if user_input == resulting_string:
-            self.entry.config(bg="green")
-            data = {"wins": 1, "losses": 0}
-        else:
-            self.entry.config(bg="red")
-            data = {"wins": 0, "losses": 1}
+    
+        response = requests.get(f"{BASE_URL}validate_answer", headers=headers, json=data)
 
-        response = requests.get(f"{BASE_URL}validate_token", headers=headers, json=data)
+        if response.status_code != 200:
+            
+
+
 
     def update(self):
         self.submit_button.config(state=tk.NORMAL, relief=tk.RAISED, background='SystemButtonFace')
 
         self.register = random.choice(list(REGISTER.keys()))
         if REGISTER[self.register] == EAX:
-            self.exercise_test_label.config(text=f"Stan bezposrednio po push EAX, \n jaka wartosc bedzie w EAX po wykonaniu pop EAX")
-        else:
-            self.exercise_test_label.config(text=f"Stan bezposrednio po push AX, \n jaka wartosc bedzie w AX po wykonaniu pop AX")
+            self.exercise_test_label.config(text="State directly after push EAX, \n what will be the value of EAX after pop EAX")
+        
+        success = self.get_new_task()
 
-        self.refresh_cells()
+        if not success:
+            messagebox.showerror("Error", "Failed to load, check internet.")
+            return
+
         for index, value in enumerate(self.cells):
             self.adress_labels[index].config(text=f"{value}".upper())
 
-        self.refresh_values()
         for index, value in enumerate(self.values):
             self.values_labels[index].config(text=f"{value}".upper())
 
@@ -99,33 +110,27 @@ class Simulator(tk.Tk):
         self.entry.delete(0, "end")
         self.entry.config(bg="white")
 
-    def refresh_cells(self):
-        self.stack_pointer : int =  random.randint(MINIMUM_ADDRESS,MAXIMUM_ADDRESS)
-        self.growth = random.choice(list(GROWTH.keys()))
-        self.cells = [
-            hex(self.stack_pointer - (3*GROWTH[self.growth]))[2:],
-            hex(self.stack_pointer - (2*GROWTH[self.growth]))[2:],
-            hex(self.stack_pointer - (1*GROWTH[self.growth]))[2:],
-            hex(self.stack_pointer)[2:],
-            hex(self.stack_pointer + (1*GROWTH[self.growth]))[2:],
-            hex(self.stack_pointer + (2*GROWTH[self.growth]))[2:],
-            hex(self.stack_pointer + (3*GROWTH[self.growth]))[2:]
-        ]
 
-    def refresh_values(self):
-        self.answer : str = str(hex(random.randint(MINIMUM_VALUE,MAXIMUM_VALUE)))[2:]
-        splitted_answer = [self.answer[i:i+2] for i in range(0, len(self.answer), 2)]
+    def get_new_task(self):
+        try:
+            response = requests.get(url=BASE_URL+"generate_task")
 
-        self.answer = splitted_answer
+        except Exception:
+            return False
+        
+        if response.status_code != 200:
+            return False
+        
+        data = response.json()
 
-        hex_integers = [random.randint(0, 255) for _ in range(3)]
-        hex_strings = [hex(num)[2:].zfill(2) for num in hex_integers]
+        self.cells = data["cells"]
+        self.values = data["values"]
+        self.stack_pointer = data["esp"]
+        self.register = data["register"]
+        self.task_id = data["task_id"]
 
-        if GROWTH[self.growth] == -1:
-            self.values =  self.answer  + hex_strings
-        else:
-            self.values =  hex_strings + self.answer[::-1]
-
-        if REGISTER[self.register] == AX:
-            self.answer = self.answer[-2::]
+        return True
+    
+    def terminate_program(self):
+        sys.exit()
 
